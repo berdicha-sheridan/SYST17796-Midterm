001e459e205fa43fd5cebf265fd9d3b22c6258de
berdicha-sheridan <xxxstels@gmail.com>
2021-07-05 14:55:01 -0400

    Initial commit: base code for the project

--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -15,21 +15,43 @@ import java.util.Scanner;
  */
 public class ArithmeticBase 
 {
- public double x,y;
+    public double x, y;
+    
+    public enum Operation
+    {
+        PLUS,
+        MINUS,
+        TIMES,
+        DIVIDE
+    };
+    
     double calculate(double x, double y) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println
+        (
+            "Enter arithmetic operation to Perform:\n" +
+            "1: PLUS\n" +
+            "2: MINUS\n" + 
+            "3: TIMES\n" +
+            "4: DIVIDE"
+        );
+        
+        int operationPosition = sc.nextInt() - 1;
+        if  (operationPosition < 1 && operationPosition > 4)
         {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
+            throw new AssertionError("Please enter a correct operation");
+        }
+        
+        switch (Operation.values()[operationPosition])
         {
-            case "PLUS":
+            case PLUS:
                 return x + y;
-            case "MINUS":
+            case MINUS:
                 return x - y;
-            case "TIMES":
+            case TIMES:
                 return x * y;
-            case "DIVIDE":
+            case DIVIDE:
                 return x / y;
             default:
                 throw new AssertionError("Unknown operations " + this);
